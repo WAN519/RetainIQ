69afd1f9eac8560cb48cd25875d9505389fb3e8e
WAN519 <maanx519@gmail.com>
2026-04-17 17:03:02 -0700

    update UI

--- retention-ui/vite.config.js
+++ retention-ui/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/RetentionAgent/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/RetainIQ/' : '/',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
